9b9b2eca42a3aeb038813e12b8c763da85f74cf4
HildeWSerpicoDev <hildegard.wiggenhorn@serpicodev.com>
2020-11-24 21:10:54 -0500

    search result git

--- lib/models/model_git_search_response.dart
+++ lib/models/model_git_search_response.dart
@@ -1,33 +1,47 @@
 
 
 
-class ModelGitSearchResponse implements Comparable {
+class ModelGitSearchResponse {
   int totalCount;
-  List<Map<String, dynamic>> files; //file name: [file pasth, repo full name]
+  List<dynamic> items;
 
-
-   
-  ModelGitSearchResponse({
-    this.totalCount, this.files
-  });
-
-  //toString() => files.forEach((file) => file.toString());
-  //'git search results: $totalCount  ';
-
-  ModelGitSearchResponse.fromJson(Map<String, dynamic> json) : 
-  TODO
-      id = json["id"], frid = json["frid"], az = json["az"], 
-      dp = json["dp"], lg = json["lg"], sc = json["sc"], exc = json["exc"];
-      //latlng = LatLng(json["latlng"]["latitude"], json["latlng"]["longitude"]);
-
-
-  @override
-  int compareTo(other) {
-    //if (this.id <= other.frid) return -1;
-    if (this.frid <= this.id && this.id <= other.id) return -1;
-    else if (this.frid > this.id && this.frid <= other.frid) return -1;
-    else return 1;
+  ModelGitSearchResponse() {
+    this.totalCount = totalCount;
+    this.items = items;
   }
 
+  toString() { 
+    String filing = "";
+    createFiles(this.items).forEach((it) => filing += it.toString());
+    return "$totalCount: $filing";
+  }
+
+  List<ModelGitFile> createFiles(List<dynamic> items) {
+    List<ModelGitFile> files = [];
+    items.forEach((item) { 
+      files.add(ModelGitFile.fromJson(item));
+    });
+    return files;
+  }
+
+  ModelGitSearchResponse.fromJson(Map<String, dynamic> json) : 
+    items = json["items"],
+    totalCount = json["total_count"];
 }
 
+class ModelGitFile {
+  String filename;
+  String path;
+  String fullName;
+
+  ModelGitFile({
+    this.filename, this.path, this.fullName
+  });
+
+  toString() => "$filename, $path, $fullName";
+
+  ModelGitFile.fromJson(Map<String, dynamic> json) : 
+    filename = json["name"],
+    path = json["path"],
+    fullName = json["repository"]["full_name"];
+}
--- lib/utils/git_search_api.dart
+++ lib/utils/git_search_api.dart
@@ -1,24 +1,15 @@
-//import 'dart:ffi';
-import 'package:flutter/material.dart';
-import 'package:xml/xml.dart';
-import 'package:flutter/services.dart' show rootBundle;
-import 'dart:math' as math;
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:latlong/latlong.dart';
+
 import 'dart:convert';
 import 'dart:io';
-import 'package:xml/xml_events.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-
-import '../models/model_segment.dart';
-import '../blocs/tmlu_bloc.dart';
+import '../models/model_git_search_response.dart';
 
 
 
 class GitSearchApi {
   
-
+  ModelGitSearchResponse searchResp;
+  List<ModelGitFile> files;
 
   getListOfFiles(String user, String searchString) async {
 
@@ -31,7 +22,9 @@ class GitSearchApi {
         final request = await HttpClient().getUrl(url);
         final HttpClientResponse response = await request.close();
         String reply = await utf8.decoder.bind(response).join();
-        print(jsonDecode(reply).toString());
+        searchResp = ModelGitSearchResponse.fromJson(jsonDecode(reply));
+        files = searchResp.createFiles(searchResp.items);
+        files.forEach((fil) => print(fil));
   
       } catch (err) {
         print('error searching github for tmlu data in utils: $err');
